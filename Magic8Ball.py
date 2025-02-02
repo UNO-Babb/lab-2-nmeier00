90f8316050ef392033873ac1ed2d34c7133052cf
nmeier00 <nicholasmeier@unomaha.edu>
2025-02-02 18:55:57 +0000

    Done with lab

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -16,3 +16,18 @@ def main():
 
 if __name__ == '__main__':
   main()
+
+
+question = input("To use the magic 8ball, ask a question:\n")
+
+answers = ["It is certain", "It is decidedly so", "Yes deffinitely",
+           "You may rely on it", "As I see it yes", "Most Likely",
+           "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+           "Ask again later", "Better not tell you now", "Cannot predict now", 
+           "Concentrate and ask again", "Don't count on it", "My reply is no",
+           "My sources say no", "Outlook not very good", "very doubtfull"]
+length = len(answers)
+r = random.random ()*length
+r = int(r)
+response = answers[r]
+print (response)
